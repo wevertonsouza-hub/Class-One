b939c75160cf14aab80dc851abbe269ff5029c08
wevertonsouza-hub <148835954+wevertonsouza-hub@users.noreply.github.com>
2024-04-23 22:13:13 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     <Text style={styles.title}> Olá </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: "blue",
+    fontSize: 100
+  }
+});
